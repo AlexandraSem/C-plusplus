663f5e2cbb96a5b52318757451b0cdc2043f7754
AlexandraSem <semashch.sasha@gmail.com>
2017-06-12 11:54:22 +0300

    Create DynArray.h

---
+++ DynArray.h
@@ -0,0 +1,48 @@
+#pragma once
+ 
+#include <initializer_list>
+ 
+template<class T>
+class DynArray
+{
+public:
+    DynArray(int capacity);
+ 
+    DynArray(std::initializer_list<T> const& list);
+private:
+    T* m_data = nullptr;
+    int m_capacity = 0;
+    int m_count = 0;
+};
+ 
+template<class T>
+DynArray<T>::DynArray(int capacity) :
+m_capacity(capacity)
+{
+    m_data = new T[m_capacity];
+}
+ 
+template<class T>
+DynArray<T>::DynArray(std::initializer_list<T> const& list)
+{
+    m_capacity = list.size();
+    std::initializer_list<T>::iterator it = list.begin();
+    m_data = new T[m_capacity];
+    T* ptr = m_data;
+    for (; it != list.end(); ++it)
+    {
+        *(ptr++) = *it;
+    }
+}
+ 
+// main.cpp
+ 
+#include "DynArray.h"
+ 
+ 
+int main()
+{
+    DynArray<int> d = {200, 300, 100};
+ 
+    return 0;
+}
